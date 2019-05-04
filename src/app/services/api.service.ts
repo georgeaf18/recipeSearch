3e0d707dc68365415b3fc768c70ef4019a30d3e7
George Almonte <georgeaf1825@gmail.com>
2019-05-03 21:00:28 -0400

    update api.service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,11 +4,11 @@ import {HttpClient} from '@angular/common/http'
 @Injectable()
 
 export class Api {
-    appId = '00a41518';
-    appKey = '482baab8eed3d4133ec335372e8171b8';
-    recipeUrl = 'https://api.edamam.com/search?q=chicken+parmesan&app_id=00a41518&app_key=&from=0&to=6'
-    fromNumber = 0;
-    toNumber = 6;
+    appId: string = '00a41518';
+    appKey: string = '482baab8eed3d4133ec335372e8171b8';
+    recipeUrl: string;
+    fromNumber: number = 0;
+    toNumber: number = 6;
     constructor( private http: HttpClient) {}
     
     getRecipe = () => {
@@ -16,6 +16,6 @@ export class Api {
             return this.http.get(this.recipeUrl = `https://api.edamam.com/search?q=pork&app_id=${this.appId}&app_key=${this.appKey}&from=${this.fromNumber}&to=${this.toNumber}&health=alcohol-free`)
             
             
-        }
+    }
     
 }
