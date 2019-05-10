f4d1e0297f6301e77d3cb40169afce2b4e68f837
George Almonte <41764662+georgeaf18@users.noreply.github.com>
2019-05-09 20:38:07 -0400

    Revert "Sarah"

--- src/app/favorites-page/favorites-page.component.ts
+++ src/app/favorites-page/favorites-page.component.ts
@@ -1,25 +1,21 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Api } from '../services/api.service';
 
-
 @Component({
-  selector: 'favorites-page',
+  selector: 'app-favorites-page',
   templateUrl: './favorites-page.component.html',
-  styleUrls: ['./favorites-page.component.css'],
-  providers: [Api]
+  styleUrls: ['./favorites-page.component.css']
 })
 export class FavoritesPageComponent implements OnInit {
  
-
 favorites: any[];
 
   constructor(private api: Api) { }
-
 
   ngOnInit() {
     this.api.recipes.subscribe(data => this.favorites = data.filter(recipe => recipe.bookmarked));
   }
 
+  
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -18,6 +18,5 @@ export class Api {
         return this.http.get(this.recipeUrl = `https://api.edamam.com/search?q=${query || ""}&app_id=${this.appId}&app_key=${this.appKey}&from=${pagFrom}&to=${pagTo}&health=${healthValue}&ingr=${numberIngr}`)
     }
 
-
     updateRecipes = newList => this._recipes.next(newList);
 }
