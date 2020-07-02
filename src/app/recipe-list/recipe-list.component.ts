170dfc89bddb13af25ee85229179f58084088668
George Almonte <georgealmonte1811@gmail.com>
2020-07-02 09:21:13 -0400

    updating portfolio

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Api } from '../services/api.service'
+import { Component, OnInit } from "@angular/core";
+import { Api } from "../services/api.service";
 
 interface RecipeInfo {
   label: string;
@@ -21,85 +21,88 @@ interface ApiData {
   hits: Recipe[];
 }
 
-
-
-
 @Component({
-  selector: 'recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css'],
+  selector: "recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
-
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
   searchInput: String;
-  health: string = 'alcohol-free';
-  numberIngr: string = '2+';
+  health: string = "alcohol-free";
+  numberIngr: string = "2+";
   pagFrom: number = 0;
   pagTo: number = 20;
   modalIndex: number;
   modalRecipe;
   modalUrl: string;
 
-  
-
-  constructor(private api: Api) { }
-  
+  constructor(private api: Api) {}
 
   ngOnInit() {
     this.api.recipes.subscribe(data => {
       //console.log('favssss', this.api.favorites)
       this.recipes = data.map(item => {
-        const recipe = this.api.favorites.find(i => i.recipe.image === item.recipe.image);
+        const recipe = this.api.favorites.find(
+          i => i.recipe.image === item.recipe.image
+        );
         item.bookmarked = false;
         if (recipe) {
           recipe.bookmarked = true;
-          return recipe
+          return recipe;
         } else {
           return item;
         }
       });
     });
+
+    this.filterRecipes();
   }
 
-  console = (index) => {
-    console.log(this.recipes[index])
-  }
+  console = index => {
+    console.log(this.recipes[index]);
+  };
 
-  show = (i) => {
+  show = i => {
     this.modalRecipe = this.recipes[i].recipe;
     this.modalUrl = this.modalRecipe.url;
-  }
+  };
 
-
-  changePag = (where) => {
-    if (where === true){
-        this.pagFrom += 20;
-        this.pagTo += 20;
-        this.filterRecipes();
-    } else if (where === false){
-      if(this.pagFrom > 0 ){
+  changePag = where => {
+    if (where === true) {
+      this.pagFrom += 20;
+      this.pagTo += 20;
+      this.filterRecipes();
+    } else if (where === false) {
+      if (this.pagFrom > 0) {
         this.pagFrom -= 20;
         this.pagTo -= 20;
         this.filterRecipes();
-      } 
+      }
     }
-  }
-
-  filterRecipes = () => {
-    this.api.getRecipe(this.searchInput, this.health, encodeURIComponent(this.numberIngr), this.pagFrom, this.pagTo ).subscribe((data: ApiData) => {
-      this.api.updateRecipes(data.hits);
-    }); 
-  }
-
-  newSearchCriteria = (event) => {
-    this.searchInput = event;
-  }
-
-  addFavorite = (recipeIndex) => {
-    this.recipes[recipeIndex].bookmarked = !this.recipes[recipeIndex].bookmarked;
-    this.api.addFavorite(this.recipes[recipeIndex]);
   };
 
+  filterRecipes = () => {
+    this.api
+      .getRecipe(
+        this.searchInput,
+        this.health,
+        encodeURIComponent(this.numberIngr),
+        this.pagFrom,
+        this.pagTo
+      )
+      .subscribe((data: ApiData) => {
+        this.api.updateRecipes(data.hits);
+      });
+  };
 
+  newSearchCriteria = event => {
+    this.searchInput = event;
+  };
+
+  addFavorite = recipeIndex => {
+    this.recipes[recipeIndex].bookmarked = !this.recipes[recipeIndex]
+      .bookmarked;
+    this.api.addFavorite(this.recipes[recipeIndex]);
+  };
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,36 +1,42 @@
-import {Injectable} from '@angular/core'
-import {HttpClient} from '@angular/common/http'
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable()
-
 export class Api {
-    appId: string = '00a41518';
-    appKey: string = '482baab8eed3d4133ec335372e8171b8';
-    recipeUrl: string;
-    favorites: any[] = [];
-    
-    private _recipes = new BehaviorSubject([]);
-    recipes = this._recipes.asObservable();
+  appId: string = "00a41518";
+  appKey: string = "482baab8eed3d4133ec335372e8171b8";
+  recipeUrl: string;
+  favorites: any[] = [];
 
-    constructor( private http: HttpClient) {}
-    
-    getRecipe = (query, healthValue, numberIngr, pagFrom, pagTo) => {
-        return this.http.get(this.recipeUrl = `https://api.edamam.com/search?q=${query || ""}&app_id=${this.appId}&app_key=${this.appKey}&from=${pagFrom}&to=${pagTo}&health=${healthValue}&ingr=${numberIngr}`)
+  private _recipes = new BehaviorSubject([]);
+  recipes = this._recipes.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  getRecipe = (query, healthValue, numberIngr, pagFrom, pagTo) => {
+    return this.http.get(
+      (this.recipeUrl = `https://api.edamam.com/search?q=${query ||
+        ""}&app_id=${this.appId}&app_key=${
+        this.appKey
+      }&from=${pagFrom}&to=${pagTo}&health=${healthValue}&ingr=${numberIngr}`)
+    );
+  };
+
+  removeFavorite = recipeToRemove => {
+    this.favorites = this.favorites.filter(
+      recipe => recipe.recipe.image !== recipeToRemove.recipe.image
+    );
+  };
+
+  addFavorite = recipe => {
+    const favorites = this.favorites;
+    if (!favorites.find(i => i.recipe.image === recipe.recipe.image)) {
+      favorites.push(recipe);
     }
+  };
 
-    removeFavorite = (recipeToRemove) => {
-        this.favorites = this.favorites.filter(recipe => recipe.recipe.image !== recipeToRemove.recipe.image);
-    }    
-
-    addFavorite = (recipe) => {
-        const favorites = this.favorites;
-        if (!favorites.find(i => i.recipe.image === recipe.recipe.image)) {
-            favorites.push(recipe);
-        }
-    }
-
-    updateRecipes = newList => {
-        this._recipes.next(newList);
-    }
+  updateRecipes = newList => {
+    this._recipes.next(newList);
+  };
 }
